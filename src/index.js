7245cc2a66cb77c21162dd7b330c8d76de983f6b
sandinofri <sandinofri92@gmail.com>
2025-03-30 15:38:54 +0700

    add new feature in footer section on beranda

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 
 let toggleMenu = document.getElementById("toggle-menu")
 let toggleButton = document.getElementById("toggle-button")
-
+let genreButton = document.getElementById("genre-button")
+let genreList = document.getElementById("genre-list")
+let genreIcon = document.getElementById("genre-icon")
+let helpIcon = document.getElementById("help-icon")
+let helpButton = document.getElementById("help-button")
+let help = document.getElementById("help")
 
 toggleButton.addEventListener("click" ,(event)=>{
     toggleMenu.classList.toggle("hidden")
@@ -13,3 +18,13 @@ document.addEventListener("click", (event) => {
         toggleMenu.classList.add("hidden");
     }
 });
+
+genreButton.addEventListener("click",()=>{
+    genreList.classList.toggle("hidden")
+    genreIcon.classList.toggle("rotate-90")
+})
+
+helpButton.addEventListener("click",()=>{
+    help.classList.toggle("hidden")
+    helpIcon.classList.toggle("rotate-90")
+})
